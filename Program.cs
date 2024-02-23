50bb252b598efe85f7c7cb07fe46d2af1f87bf97
goomatt33 <goomatt33@gmail.com>
2024-02-23 12:27:50 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string workingDir = ""; // Replace with the dir to remove empty dirs from
+
+var dirs = Directory.GetDirectories(workingDir);
+
+foreach (var dir in dirs)
+{
+    if(!Directory.EnumerateFileSystemEntries(dir).Any())
+    {
+        Directory.Delete(dir, true);
+    }
+}
